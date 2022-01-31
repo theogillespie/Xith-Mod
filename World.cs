a6bd485dd02b6113f3e58f8235b24151720cd61f
meepsauce <gillespietheo@gmail.com>
2022-01-31 14:06:04 +0000

    wrote ideas in associated folders, created template for World class

---
+++ World.cs
@@ -0,0 +1,14 @@
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.World.Generation;
+using Microsoft.Xna.Framework;
+
+namespace SophmoreProject {
+    public class World : ModWorld {
+        
+    }
+}
